1cc0ee32aa99bee0e1d36f6cb496a49f87297599
taysontey <taysontey@gmail.com>
2016-03-11 18:08:05 -0300

    Perfil Admin/Clientes(1)

---
+++ Projeto.Web/Areas/Admin/Controllers/FornecedorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class FornecedorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
